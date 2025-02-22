dd84e02304b81338830e75bb4339018631cbce54
SheikhAmin785 <shakil785ks@gmail.com>
2025-02-22 12:19:50 +0600

    initial

---
+++ OracleDatabaseConnectionApps/OracleDatabaseConnectionApps/Controllers/DataController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OracleDatabaseConnectionApps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private readonly DataAccess _dataAccess;
+
+        public DataController(DataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            var query = "select * from policy";
+            var result = _dataAccess.ExecuteQuery(query);
+            return Ok(result);
+        }
+
+        // Add other action methods as needed
+    }
+}
---
+++ OracleDatabaseConnectionApps/OracleDatabaseConnectionApps/DataAccess.cs
@@ -0,0 +1,28 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace OracleDatabaseConnectionApps
+{
+    public class DataAccess
+    {
+        private readonly string _connectionString;
+
+        public DataAccess(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            using (var connection = new OracleConnection(_connectionString))
+            {
+                var command = new OracleCommand(query, connection);
+                var adapter = new OracleDataAdapter(command);
+                var dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
+    }
+}
+    
